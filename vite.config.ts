f4db4b0eaf32121247406be3978a94b307b8758f
c0by <jsamin3@gmail.com>
2025-09-24 20:09:50 +0100

    #1.2 Update config for new repo name

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:"/guacamole",
+  base:"GPU-overheat.github.io",
   plugins: [react(), tailwindcss()],
 })
